7726f30c49664ad863480d031a4bd6af34329950
Esteban Saez <e.saez07@ufromail.cl>
2022-09-21 10:52:37 -0300

    ultimo commit

--- src/main/java/Sismos.java
+++ src/main/java/Sismos.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class Sismos {
     public static void main(String[] args) {
+
     }
 
-    public static int nDias(){
-        Scanner entrada= new Scanner(System.in);
+    public static int nDias() {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Ingresa el numero de Dias");
         int dias;
         do {
@@ -20,26 +21,47 @@ public class Sismos {
         } while (true);
         return dias;
     }
+
     public static void mostrarError() {
         System.err.println("Opción inválida");
         System.err.println("Ingrese un número válido");
     }
 
-    public static double[][] llenarMatriz(double[][] matriz){
-        for (int x=0; x < matriz.length; x++) {
-            for (int y=0; y < matriz[x].length; y++) {
-                double num=(Math.random()*9+0.5);
+
+    public static double[][] llenarMatriz(double[][] matriz) {
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                double num = (Math.random() * 9 + 0.5);
                 matriz[x][y] = Math.round(num * 10) / 10d;
             }
         }
         return matriz;
     }
-    public static void mostrarMatriz(double [][] matriz){
-        for (int x=0; x < matriz.length; x++) {
-            for (int y=0; y < matriz[x].length; y++) {
-                System.out.print ("["+matriz[x][y]+"]");
+
+    public static void mostrarMatriz(double[][] matriz) {
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                System.out.print("[" + matriz[x][y] + "]");
             }
             System.out.println("");
+        }
+    }
+
+    public static void escSismica(double[][] matriz) {
+        int dia = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (j < matriz[i].length - 4) {
+                    if (matriz[i][j] > 6.0 && matriz[i][j + 1] > 6.0 && matriz[i][j + 2] > 6.0 && matriz[i][j + 3] > 6.0) {
+                        dia = i + 1;
+                    }
+                }
+            }
+        }
+        if (dia == 0) {
+            System.out.println("No hubo escalada sismica");
+        } else {
+            System.out.println("ESCALADA SISMICA EL DIA " + dia);
         }
     }
 }
